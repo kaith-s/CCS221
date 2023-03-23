fad7cd0c64412082d2cba03f421f1e7c143d7ac5
unknown <theakaithfranchette.selerio@wvsu.edu.ph>
2023-03-23 21:24:26 +0800

    act3

--- pages/activity_3.py
+++ pages/activity_3.py
@@ -14,63 +14,75 @@ def read_image(path):
     img = cv2.imread(path)
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def translate(img):
+def translation(img, tx, ty):
     rows, cols = img.shape[:2]
-    M = np.float32([[1, 0, 60],
-                [0, 1, 50],
-                [0, 0, 1]])
+    m_translation_ =np.float32([[1,0, tx],
+                        [0,1 , ty],
+                        [0,0,1]])
 
-    translated_img_=cv2.warpPerspective(img, M, (cols, rows))
+    translated_img_= cv2.warpPerspective(img,m_translation_,(cols, rows))
     return translated_img_
 
+def rotation(img, rotx):
 
-def rotate(img):
-
-    angle= np.radians(10)
+    angle =np.radians(10)
     rows, cols = img.shape[:2]
-    m_rotation_ = np.float32([[np.cos(angle), -(np.sin(angle)),0],[np.sin(angle),np.cos(angle),0],[0,0,1]])
-    rotated_img_ = cv2.warpPerspective(img ,m_rotation_,(int(cols),int(rows)))
+    m_rotation_=np.float32([[np.cos(angle), -(np.sin(angle)), 0],
+                        [np.sin(angle), np.cos(angle),0],
+                        [0,0,1]])
 
-    m_rotation_=cv2.getRotationMatrix2D((cols/2,rows/2),90,1)
-
+    m_rotation_=cv2.getRotationMatrix2D((cols/2,rows/2), rotx, 1)
     rotated_img_ = cv2.warpAffine(img,m_rotation_,(cols,rows))
     return rotated_img_
 
-
-
-def scaling(img):
+def scaling_img(img, scaleX, scaleY):
     rows, cols = img.shape[:2]
-    m_scaling_=np.float32([[1.5,0,0],
-                       [0,1.8,0],
+    m_scaling_=np.float32([[scaleX ,0 ,0],
+                       [0, scaleY ,0],
                        [0,0,1]])
     scaled_img_ =cv2.warpPerspective(img,m_scaling_,(cols*2,rows*2))
     return scaled_img_
 
-
-
-def reflection(img):
+def reflection_v(img):
     rows, cols = img.shape[:2]
     m_reflection_ =np.float32([[1,0,0],
-                          [0,-1,rows],
+                          [0,-1, rows],
                           [0,0,1]])
     reflected_img_ =cv2.warpPerspective(img,m_reflection_,(int(cols),int (rows)))
     return reflected_img_
 
-
-def shear(img):
+def reflection_h(img):
     rows, cols = img.shape[:2]
-    m_shearing_x=np.float32([[1,0.5,0],
+    m_reflection_ =np.float32([[-1, 0, cols],
+                          [0, 1, 0],
+                          [0, 0, 1]])
+    reflected_img_ =cv2.warpPerspective(img,m_reflection_,(int(cols),int (rows)))
+    return reflected_img_
+
+def shear_X(img, shearX):
+    rows, cols = img.shape[:2]
+    m_shearing_x=np.float32([[1, shearX ,0],
                          [0,1,0],
                          [0,0,1]])
 
-    sheared_img_x = cv2.warpPerspective(img,m_shearing_x,(int(cols*1.5),int(rows*1.5)))
+    sheared_img_x = cv2.warpPerspective(img, m_shearing_x,(int(cols*1.5),int(rows*1.5)))
+
+    return sheared_img_x
+
+def shear_Y(img, shearY):
+    rows, cols = img.shape[:2]
+    m_shearing_x=np.float32([[1, 0 ,0],
+                         [ shearY , 1 ,0],
+                         [0, 0, 1]])
+
+    sheared_img_x = cv2.warpPerspective(img, m_shearing_x,(int(cols*1.5),int(rows*1.5)))
 
     return sheared_img_x
 
 def main():
-    st.title("ACTIVITY 3 - IMAGE TRANSFORMATIONS")
+    st.title ("Activity 3: IMAGE TRANSFORMATIONS")
 
-    # file upload
+      # file upload
     uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         # read image
@@ -80,32 +92,66 @@ def main():
     else:
         img = read_image()
 
-    # perform transformations
-    translated_img_ = translate(img)
-    rotated_img_ = rotate(img)
-    scaled_img_ = scaling(img)
-    reflected_img_ = reflection(img)
-    sheared_img_x = shear(img)
+    #The following lines calls each of the functions for specific transformations of the images.
+    tx = int(input("Enter the value to shift in X-axis: ")) 
+    ty = int(input("Enter the value to shift in Y-axis: "))
+    rotx = int(input("Enter value to rotate the image in degrees: "))
+    scaleX = int(input("Enter the value to scale in X-Axis: "))
+    scaleY = int(input("Enter the value to scale in Y-Axis: "))
+    shearX = float(input("Enter the value to shear in X-Axis: "))
+    shearY = float(input("Enter the value to shear in Y-Axis: "))
 
-    # show images
-    st.subheader("Original Image")
-    st.image(img)
+for i in range(1, 6): 
+        img_path = f"haha.png"
+        img = cv2.imread(img_path)
+        cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    st.subheader("Translated Image")
-    st.image(translated_img_)
+        #The following lines calls each of the functions for specific transformations of the images.
+        translated_img_ = translation(img, tx=int(input("Enter the value to shift in X-axis: ")) , ty=int(input("Enter the value to shift in Y-axis: ")))
+        rotated_img_ = rotation(img, rotx=int(input("Enter value to rotate the image in degrees: ")))
+        scaled_img_ = scaling_img(img,  scaleX = int(input("Enter the value to scale in X-Axis: ")), scaleY = int(input("Enter the value to scale in Y-Axis: ")))
+        reflected_h = reflection_h(img)
+        reflected_v = reflection_v(img)
+        sheared_img_x = shear_X(img,  shearX = float(input("Enter the value to shear in X-Axis: ")))
+        sheared_img_y = shear_Y(img, shearY = float(input("Enter the value to shear in Y-Axis: ")))
 
-    st.subheader("Rotated Image")
-    st.image(rotated_img_)
+        
+        fig, axs = plt.subplots(2, 4, figsize=(18, 14))
+        fig = plt.gcf()
+        fig.canvas.manager.set_window_title('OpenCV Transformations') 
 
-    st.subheader("Scaled Image")
-    st.image(scaled_img_)
+        axs[0, 0].imshow(img)
+        axs[0, 0].set_title("Original Image")
+        axs[0, 0].axis("off")
 
-    st.subheader("Reflected Image")
-    st.image(reflected_img_)
+        axs[0, 1].imshow(translated_img_)
+        axs[0, 1].set_title("Translated Image")
+        axs[0, 1].axis("off")
 
-    st.subheader("Sheared Image")
-    st.image(sheared_img_x)
+        axs[0, 2].imshow(rotated_img_)
+        axs[0, 2].set_title("Rotated Image")
+        axs[0, 2].axis("off")
 
-    plt.show()
-    
-main()
+        axs[0, 3].imshow(scaled_img_)
+        axs[0, 3].set_title("Scaled Image")
+        axs[0, 3].axis("off")
+
+        axs[1, 0].imshow(reflected_h)
+        axs[1, 0].set_title("Reflected Image (Horizontal)")
+        axs[1, 0].axis("off")
+
+        axs[1, 1].imshow(reflected_v)
+        axs[1, 1].set_title("Reflected Image (Vertical)")
+        axs[1, 1].axis("off")
+
+        axs[1, 2].imshow(sheared_img_x)
+        axs[1, 2].set_title("Sheared Horizontally(X)")
+        axs[1, 2].axis("off")
+
+        axs[1, 3].imshow(sheared_img_y)
+        axs[1, 3].set_title("Sheared Vertically(Y)")
+        axs[1, 3].axis("off")
+
+        plt.show() 
+
+main ()
