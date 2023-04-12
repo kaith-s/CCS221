ddf305a77aa8047ef35e11a02e05b3ae17b7a12c
unknown <theakaithfranchette.selerio@wvsu.edu.ph>
2023-04-12 10:40:43 +0800

    act4

--- pages/activity4_4_3DShapes.py
+++ pages/activity4_4_3DShapes.py
@@ -257,6 +257,7 @@ def main ():
         
           translation_amount = tf.constant ([x,y,z], dtype=tf.float32)
           translated_points = translation_amount + points
+          _plt_basic_object_ (translated_points.numpy ())
           st.subheader("Cube")
           st.pyplot()
 
